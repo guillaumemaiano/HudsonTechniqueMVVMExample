59f36e2041e84d3866d1b6bc688a5491af71a729
GEM <guillaume@maiano.fr>
2019-12-17 11:27:26 +0100

    Initial Commit

---
+++ MVVMBindObservable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMBindObservable
+//
+//  Created by guillaume MAIANO on 17/12/2019.
+//  Copyright © 2019 guillaume MAIANO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
